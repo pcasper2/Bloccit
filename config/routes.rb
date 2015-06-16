8b3660f0cb476691a8175717cf2ffc996b17ac69
Paul Casperson <casperson.paul@gmail.com>
2015-06-16 14:02:01 -0500

    Crud assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   resources :posts
 
   get 'about' => 'welcome#about'
 
   get 'welcome/contact'
+
+  resources :advertisements
 
   root to: 'welcome#index'
 end
