6cfbcd289325d52b117bfe11365dc174e49790e1
Paul Casperson <casperson.paul@gmail.com>
2015-06-15 22:13:13 -0500

    finished seed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@ require 'faker'
     )
 end
 posts = Post.all
-  
+
+
 #create comments
 100.times do
   Comment.create!(
@@ -17,6 +18,34 @@ posts = Post.all
     )
 end
 
+#creating a post after all the other posts
+#it should only be created once
+
+if Post.exists?(:title => "Blackhawks!!!") == false
+    my_post = Post.create!(
+      title: "Blackhawks!!!",
+      body: "They are about to win the Stanley Cup!"
+      )
+end
+
+#creating a comment to attach to my blackhawks post
+#actually coded this during the final seconds of 
+#the Stanley Cup final.
+
+if Comment.exists?(:post => my_post) == false
+  my_comment = Comment.create!(
+    post: my_post,
+    body: "They actually won it! 2-0 over the TB Lightning! 3 cups in 6 years, baby!"
+    )
+end
+
+
+#tested to see that i only inputed once
+if Post.exists?(:title => "Blackhawks!!!")
+  puts "hey i made my own post only once"
+end
+
+  
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
