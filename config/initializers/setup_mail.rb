6945289d5bd33996708e65f860e214907da7f85b
Paul Casperson <casperson.paul@gmail.com>
2015-06-25 16:10:23 -0500

    implementing mailgun

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:              'smtp.sendgrid.net',
+    address:              'smtp.mailgun.org',
     port:                 '587',
     authentication:       :plain,
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    domain:               'heroku.com',
+    user_name:            ENV['MAILGUN_SMTP_USERNAME'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:               'app0bf2e59d708c4f379d05a04c70a34d0f.mailgun.org',
     enable_starttls_auto: true
   }
 end
