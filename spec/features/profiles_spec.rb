eab5aa97db6ad4092668b14f8dde8ae98d3c6268
Paul Casperson <casperson.paul@gmail.com>
2015-08-24 13:14:06 -0500

    public profile assignment

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 describe "Visiting Profiles" do 
   include TestFactories
@@ -21,4 +23,36 @@ describe "Visiting Profiles" do
       expect( page ).to have_content(@comment.body)
     end
   end
+
+  before do
+    login_as(@user, :scope => :user)
+  end
+
+
+  describe "signed in" do
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
+
+
+
+  describe "visiting own profile" do
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq(user_path(@user))
+
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
 end
+
+
+
