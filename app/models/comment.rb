4a9808869ea4de547fd62c005905a331c00c46cc
Paul Casperson <casperson.paul@gmail.com>
2015-08-28 14:14:17 -0500

    create assignment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   after_create :send_favorite_emails
-  default_scope { order('updated_at DESC') }
+  default_scope { order('updated_at ASC') }
   
   private
  
